64db897a1ab9e1596ff1a8aa89e461a84be1ae35
kkollsga <kkollsg@gmail.com>
2024-04-12 10:36:40 +0200

    Debug

--- src/lib.rs
+++ src/lib.rs
@@ -16,23 +16,23 @@ fn excel_extract(_py: Python, file_paths: &PyList, extraction_details: &PyList, 
         p.extract::<String>()
             .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(format!("Error extracting string: {}", e)))
     }).collect::<PyResult<Vec<String>>>()?;
-
-    // Convert Python extraction details into Serde JSON Value using the utility function
     let extraction_details_serde = pylist_to_json(extraction_details)?;
 
     // Create a new Tokio runtime to run async process_files function
     let rt = runtime::Runtime::new().unwrap();
     let results = rt.block_on(async {
-        // Call process_files asynchronously with the collected parameters
-        process_files(file_paths, extraction_details_serde, num_workers.unwrap_or(5)).await
-    }).map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(format!("Error processing files: {}", e)))?;
+        let (results, mut progress_receiver) = process_files(file_paths, extraction_details_serde, num_workers.unwrap_or(5)).await?;
+        while let Some(progress) = progress_receiver.recv().await {
+            println!("Progress: {}", progress);
+        }
+        Ok(results)
+    }).map_err(|e: anyhow::Error| pyo3::exceptions::PyRuntimeError::new_err(format!("Error processing files: {}", e)))?;
+
 
     // Convert the Serde JSON Value results into JSON strings
     let json_strings = results.into_iter().map(|val| {
         to_string(&val).map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(format!("Error converting to JSON string: {}", e)))
     }).collect::<PyResult<Vec<String>>>()?;
-
-    // Return the JSON strings to Python
     Ok(json_strings)
 }
 
--- src/parallel.rs
+++ src/parallel.rs
@@ -1,25 +1,26 @@
-use tokio::sync::Semaphore; // Import Semaphore from tokio::sync
+use tokio::sync::{Semaphore, mpsc}; // Import Semaphore from tokio::sync
 use std::sync::Arc; // Import Arc for creating reference-counted pointers
-use std::io::{self, Write};
+
 use futures::stream::{FuturesUnordered, StreamExt}; // Import FuturesUnordered and StreamExt for managing and polling futures
 use serde_json::Value; // Import serde_json::Value
 use crate::read_excel::process_file;
 use anyhow::Result; // Use anyhow::Result for simplified error handling
 use std::time::Instant; // Import Instant to measure elapsed time
 
-pub async fn process_files(file_paths: Vec<String>, extraction_details: Vec<Value>, num_workers: usize) -> Result<Vec<Value>> {
+pub async fn process_files(file_paths: Vec<String>, extraction_details: Vec<Value>, num_workers: usize) -> Result<(Vec<Value>, mpsc::Receiver<String>)> {
     println!("Processing files!");
     let semaphore = Arc::new(Semaphore::new(num_workers)); // Wrap Semaphore in an Arc for shared ownership
+    let (progress_sender, progress_receiver) = mpsc::channel(100);
 
     let mut futures = FuturesUnordered::new(); // Create a FuturesUnordered collection for managing futures
     let start_time = Instant::now(); // Record the start time for logging progress
 
-    for (_index, path_str) in file_paths.into_iter().enumerate() {
+    for path_str in file_paths.into_iter() {
         let path_str_clone = path_str.clone();
         let details_clone = extraction_details.clone(); // Clone extraction_details for each async task
-        let sem_clone = semaphore.clone(); // Clone the Arc, not the Semaphore itself
+        let semaphore_clone = semaphore.clone();
 
-        let permit = sem_clone.acquire_owned().await.unwrap(); // Acquire a permit from the semaphore
+        let permit = semaphore_clone.acquire_owned().await.unwrap(); // Acquire a permit from the semaphore
 
         futures.push(tokio::spawn(async move {
             // Once a permit is acquired, push the task into FuturesUnordered
@@ -37,20 +38,18 @@ pub async fn process_files(file_paths: Vec<String>, extraction_details: Vec<Valu
         match res {
             Ok(Ok(value)) => {
                 results.push(value); // Handle the double Result layer (tokio::spawn + process_file)
-                log_progress(&results, total_files, &start_time); // Log progress after each file is processed
-                io::stdout().flush().unwrap();
+                log_progress(&results, total_files, &start_time, progress_sender.clone()).await; // Log progress after each file is processed
             },
             Ok(Err(e)) => return Err(e.into()), // Convert the inner error to the function's error type
             Err(e) => return Err(anyhow::Error::new(e)), // Convert the JoinError to the function's error type
         }
     }
-
     println!("All files processed. Total time: {:.2?}", start_time.elapsed()); // Log the completion of all tasks
-    Ok(results) // Return the results if successful
+    Ok((results, progress_receiver))
 }
 
 // Function to log the progress of file processing
-fn log_progress(results: &[Value], total_files: usize, start_time: &Instant) {
+async fn log_progress(results: &[Value], total_files: usize, start_time: &Instant, progress_sender: mpsc::Sender<String>) {
     let processed_files = results.len();
     let files_left = total_files - processed_files;
     let avg_time_per_file = if processed_files > 0 {
@@ -59,9 +58,10 @@ fn log_progress(results: &[Value], total_files: usize, start_time: &Instant) {
         0.0 // Avoid division by zero if no files have been processed yet
     };
     let estimated_time_left = avg_time_per_file * files_left as f64;
-    println!(
+    let msg = format!(
         " Progress: {}/{} files. Avg: {:.2}s. Time left: {:.2}s.",
         processed_files, total_files, estimated_time_left, avg_time_per_file
     );
+    progress_sender.send(msg).await.unwrap();
 }
 
